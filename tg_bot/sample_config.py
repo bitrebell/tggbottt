c243cd87c82c967ed9329244bc9023b97edb67ac
ADIL SHIEKH <114225192+Darkranger00@users.noreply.github.com>
2023-03-08 13:02:36 +0530

    Create sample_config.py

---
+++ tg_bot/sample_config.py
@@ -0,0 +1,60 @@
+class Development(Config):
+  
+  
+    API_ID = 10738943
+    API_HASH = "da61e3a08b5ac78ce28b4a4cd854aeec"
+    TOKEN = "6009111407:AAGfId8nCk6BRQdq1cR4Bo1LUQPMqSjLZ68"
+    OWNER_ID = 5716978554
+    OWNER_USERNAME = "aadillllll"
+    OWNER_USERNAME = "CrushAssistant"
+    SUPPORT_CHAT = "CrushAssistant"
+    MESSAGE_DUMP = '-1001606613577'
+    USE_MESSAGE_DUMP = True
+    JOIN_LOGGER = (-1001306127048)
+    EVENT_LOGS = (-1001677106070)
+
+    SQLALCHEMY_DATABASE_URI = ""
+    MONGO_DB_URI = "mongodb+srv://adil00:12345@cluster0.fyt5yoa.mongodb.net/?retryWrites=true&w=majority"
+    LOAD = []
+    NO_LOAD = ["translation"]
+    WEBHOOK = False
+    INFOPIC = True
+    URL = None
+
+    # OPTIONAL
+    ##List of id's -  (not usernames) for users which have sudo access to the bot.
+    DRAGONS = 5716978554
+    ##List of id's - (not usernames) for developers who will have the same perms as the owner
+    DEV_USERS = 5716978554
+    ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
+    DEMONS = 5716978554
+    # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
+    TIGERS = 5716978554
+    WOLVES = 5716978554
+
+    DONATION_LINK = "https://t.me/aadillllll"
+    CERT_PATH = None
+    PORT = 5000
+    DEL_CMDS = True
+    STRICT_GBAN = True
+    WORKERS = (
+        8  # Number of subthreads to use. Set as number of threads your processor uses
+    )
+    BAN_STICKER = (
+        "CAACAgUAAxkBAAEDafNhq5Z0DegqVzauwSighMw5cPWp8QACVgQAAuUG0FRXfCEuBziNzCIE"
+    )
+    ALLOW_EXCL = True
+    CASH_API_KEY = "SHOY214OCQ32QEDS"
+    TIME_API_KEY = "ALX2ISMBZXZX"
+    BL_CHATS = []
+    SPAMMERS = None
+    ALLOW_CHATS = True
+    START_IMG = "https://te.legra.ph/file/c899d55728cee2e8fe344.jpg"
+    HEROKU_API_KEY = None
+    HEROKU_APP_NAME = None
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    ARQ_API_KEY = "LJMETG-DPHBCX-DGHJCD-TMFIGB-ARQ"
+    ARQ_API_URL = "https://arq.hamker.in"
+    ALLOW_EXCL = None
+    BL_CHATS = []  # List of groups that you want blacklisted.
+    SPAMMERS = None
